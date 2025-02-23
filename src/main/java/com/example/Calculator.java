733569e05177b801d91a9788e1a912ec9d8c8ed5
colin121502 <122415898+colin121502@users.noreply.github.com>
2025-02-23 17:02:31 -0500

    Testing Jenkins auto-build

--- src/main/java/com/example/Calculator.java
+++ src/main/java/com/example/Calculator.java
@@ -1,6 +1,7 @@
 package com.example;
 
 public class Calculator {
+
     public int add(int a, int b) {
         return a + b;
     }
@@ -9,9 +10,23 @@ public class Calculator {
         return a - b;
     }
 
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public double divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Cannot divide by zero");
+        }
+        return (double) a / b;
+    }
+
     public static void main(String[] args) {
         Calculator calc = new Calculator();
+        System.out.println("Calculator is running...");
         System.out.println("Sum: " + calc.add(5, 3));
         System.out.println("Difference: " + calc.subtract(5, 3));
+        System.out.println("Product: " + calc.multiply(5, 3));
+        System.out.println("Quotient: " + calc.divide(6, 3));
     }
 }
